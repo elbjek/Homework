789fe00ff04387ad7c7463c0e09cc5dc39609a47
elbjek <lenabjekovic@hotmaill.com>
2018-05-12 14:30:14 +0200

    hopefully final push

--- scripts/domain/slider.js
+++ scripts/domain/slider.js
@@ -4,6 +4,27 @@ $(document).ready(function(){
   $('.slick').slick({
     autoplay:true,
     autoplaySpeed:1200,
-    mobileFirst:true
+    mobileFirst:true,
+    dots:true,
+    arrows:false,
+    centerMode:false,
+    centerPadding: '0px',
+    infinite:false,
+    fade:true,
+    responsive: [
+      {breakpoint:3000,
+      settings: {slidesToShow:1,slidesToScroll:1}
+      },
+      {breakpoint:1000,
+      settings: {slidesToShow:1,slidesToScroll:1}
+      },
+      {breakpoint:750,
+      settings: {slidesToShow:1,slidesToScroll:1}
+      },
+      {breakpoint:500,
+      settings: {slidesToShow:1, slidesToScroll:1}
+      }
+      ]
   });
+
 });
