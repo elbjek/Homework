c2ba8c24398907cc76596eb97f46816935b26d06
elbjek <lenabjekovic@hotmaill.com>
2018-05-12 02:05:47 +0200

    first push

---
+++ scripts/app.js
@@ -0,0 +1,11 @@
+// Domain
+import './domain/fadeOuter';
+import './domain/slider';
+
+import 'bootstrap';
+import 'bootstrap/js/dist/util';
+import 'bootstrap/js/dist/dropdown';
+
+// Global Vendors
+
+
---
+++ scripts/domain/slider.js
@@ -0,0 +1,9 @@
+import '../vendor/slick.min';
+
+$(document).ready(function(){
+  $('.slick').slick({
+    autoplay:true,
+    autoplaySpeed:1200,
+    mobileFirst:true
+  });
+});
